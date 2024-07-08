5fc5007d13285ed90e8ecbea7c62b6f8a7f63d3e
Xybron <ronaldnettey@yahoo.com>
2024-07-08 16:31:04 +0000

    updated the application layer notier

--- application_layer/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/application/{{#snakeCase}}{{name}}{{/snakeCase}}.notifier.dart
+++ application_layer/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}/application/{{#snakeCase}}{{name}}{{/snakeCase}}.notifier.dart
@@ -2,12 +2,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:injectable/injectable.dart';
 
 final {{name.camelCase()}}Provider = StateNotifierProvider<{{name.pascalCase()}}Notifier, {{name.pascalCase()}}State>(
-    (ref) => getIt<{{name.pascalCase()}}Notifier>(param1: ref.read));
+    (ref) => getIt<{{name.pascalCase()}}Notifier>());
 
 @injectable
 class {{name.pascalCase()}}Notifier extends StateNotifier<{{name.pascalCase()}}State> {
   final I{{name.pascalCase()}}Facade _i{{name.pascalCase()}}Facade;
-  final Reader read;
   {{name.pascalCase()}}Notifier(@factoryParam this.read, this._i{{name.pascalCase()}}Facade)
       : super({{name.pascalCase()}}State.initial());
 }
