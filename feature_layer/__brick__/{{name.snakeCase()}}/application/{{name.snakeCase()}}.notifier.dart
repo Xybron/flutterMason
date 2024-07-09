b23ed101e2b9c9a56532e9820e9789c1cae3df63
Xybron <ronaldnettey@yahoo.com>
2024-07-09 12:08:25 +0000

    updated the feature brick

---
+++ feature_layer/__brick__/{{name.snakeCase()}}/application/{{name.snakeCase()}}.notifier.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:injectable/injectable.dart';
+
+final {{name.camelCase()}}Provider = StateNotifierProvider<{{name.pascalCase()}}Notifier, {{name.pascalCase()}}State>(
+    (ref) => getIt<{{name.pascalCase()}}Notifier>());
+
+@injectable
+class {{name.pascalCase()}}Notifier extends StateNotifier<{{name.pascalCase()}}State> {
+  final I{{name.pascalCase()}}Facade _i{{name.pascalCase()}}Facade;
+  {{name.pascalCase()}}Notifier(this._i{{name.pascalCase()}}Facade)
+      : super({{name.pascalCase()}}State.initial());
+}
---
+++ feature_layer/__brick__/{{name.snakeCase()}}/domain/{{name.snakeCase()}}.failure.dart
@@ -0,0 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part '{{name.snakeCase()}}.failure.freezed.dart';
+
+@freezed
+class {{name.pascalCase()}}Failure with _${{name.pascalCase()}}Failure {
+  // An example failure. 
+  // const factory {{name.pascalCase()}}Failure.example() = Example;
+}
